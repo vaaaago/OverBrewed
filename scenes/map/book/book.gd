5de5b9d4b197b473a6082a720a300c6495f608fa
Linna <catalina.mu.ol@gmail.com>
2025-05-12 15:36:33 -0400

    feat: se agregó el libro de recetas con un script inicial para interactuar con él.

---
+++ scenes/map/book/book.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+@onready var open_book := $LibroUI/TextureRect
+@onready var interaction_area := $Area2D
+@onready var texto := $LibroUI/RichTextLabel
+@onready var libro_ui := $LibroUI
+var player_in_range := false
+var libro_abierto = false
+
+func _ready():
+	open_book.visible = false
+	#texto.text = "[center]Era una noche oscura y tormentosa...\n\nContinúa en la siguiente página."
+	
+	
+	
+func _process(delta):
+	if player_in_range and Input.is_action_just_pressed("interact"): # define "abrir_libro" como tecla E en Input Map
+		#open_book.visible = not open_book.visible
+		libro_abierto = !libro_abierto
+		libro_ui.visible = libro_abierto
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		player_in_range = true
+		
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player_in_range = false
+		libro_abierto = false
+		libro_ui.visible = false
