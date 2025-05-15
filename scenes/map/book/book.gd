136ea14e6d83996189e8b09054016d8d175fa251
Linna <catalina.mu.ol@gmail.com>
2025-05-15 19:21:11 -0400

    fix: se manda la señal para abrir el libro de recetas

--- scenes/map/book/book.gd
+++ scenes/map/book/book.gd
@@ -8,7 +8,7 @@ var player_in_range := false
 var libro_abierto = false
 
 func _ready():
-	open_book.visible = false
+	libro_ui.visible = false
 	#texto.text = "[center]Era una noche oscura y tormentosa...\n\nContinúa en la siguiente página."
 	
 	
@@ -30,3 +30,11 @@ func _on_body_exited(body):
 		player_in_range = false
 		libro_abierto = false
 		libro_ui.visible = false
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
