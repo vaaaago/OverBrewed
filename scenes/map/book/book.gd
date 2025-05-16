25a2b247c19864e4632e842df9ab588e75d11fbf
diego5714 <diego5714@proton.me>
2025-05-16 12:21:41 -0400

    Libro: Arreglada deteccion de gatos.

--- scenes/map/book/book.gd
+++ scenes/map/book/book.gd
@@ -5,30 +5,25 @@ extends Node2D
 @onready var texto := $LibroUI/RichTextLabel
 @onready var libro_ui := $LibroUI
 
-
 var player_in_range := false
 var libro_abierto = false
 
 func _ready():
 	libro_ui.visible = false
 	#texto.text = "[center]Era una noche oscura y tormentosa...\n\nContinúa en la siguiente página."
-	
-	
-	
+
 func _process(delta):
 	if player_in_range and Input.is_action_just_pressed("interact"): # define "abrir_libro" como tecla E en Input Map
 		#open_book.visible = not open_book.visible
 		libro_abierto = !libro_abierto
 		libro_ui.visible = libro_abierto
 
-
 func _on_body_entered(body):
-	if body.name == "Player":
+	if body.is_class("CharacterBody2D"):
 		player_in_range = true
-		
 
-func _on_body_exited(body):
-	if body.name == "Player":
+func _on_body_exited(body: Node2D):
+	if body.is_class("CharacterBody2D"):
 		player_in_range = false
 		libro_abierto = false
 		libro_ui.visible = false
