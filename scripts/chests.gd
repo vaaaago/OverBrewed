c17dc7dac64da2b66ef507ededd1550f7816b27c
diego5714 <diego5714@proton.me>
2025-07-11 02:48:08 -0400

    Agregados frames de textura a los cofres

--- scripts/chests.gd
+++ scripts/chests.gd
@@ -1,4 +1,9 @@
 extends ItemSource
 
+@onready var texture_rect: TextureRect = $MarginContainer/TextureRect/MarginContainer/TextureRect
+
+func _ready() -> void:
+	texture_rect.texture = item_type.texture
+
 func play_pickup_animation() -> void:
 	anim.play("open_chest")
